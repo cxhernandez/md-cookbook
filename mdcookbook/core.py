5d9b3464bd93d9547bd5310937047c7c84b09fce
Carlos Hernandez <cxh@stanford.edu>
2015-04-26 23:26:23 -0700

    added seed_from_seq

--- mdcookbook/core.py
+++ mdcookbook/core.py
@@ -4,7 +4,8 @@ from simtk.unit import picoseconds, femtoseconds, nanometers
 from simtk.unit import kelvin, molar, bar
 
 try:
-    from rosetta import Pose, pose_from_pdb, get_fa_scorefxn, FastRelax
+    from rosetta import Pose, FastRelax
+    from rosetta import pose_from_pdb, pose_from_seq, get_fa_scorefxn
     from toolbox import mutate_residue as mutate
 except ImportError:
     FastRelax = get_fa_scorefxn = pose_from_pdb = mutate = None
@@ -41,6 +42,18 @@ def model(pdb, mutpos=None, mut=None, cap=True):
         pose = addCaps(pose)
     if mutpos:
         pose = mutate(pose, mutpos, mut)
+    relax.apply(pose)
+    return pose
+
+
+def model_from_seq(seq, cap=True):
+    if pose_from_seq is None:
+        raise ImportError('Could not find PyRosetta.')
+    scorefxn = get_fa_scorefxn()
+    relax = FastRelax(scorefxn, 15)
+    pose = pose_from_seq(seq)
+    if cap:
+        pose = addCaps(pose)
     relax.apply(pose)
     return pose
 
